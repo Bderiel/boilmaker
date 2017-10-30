fbbf1117897d7ea62b345ded22fc951f66f9bdf4
Devon JohnBaptiste <devondevon@outlook.com>
2017-10-30 18:30:06 -0400

    scss init

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,8 @@
 import { render } from 'react-dom';
 import React from 'react';
-import { Provider } from 'redux';
+import { Provider } from 'react-redux';
 import store from './store';
+import './index.scss';
 
 render(
   <Provider store={store}>
